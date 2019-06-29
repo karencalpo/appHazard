a2b8459aaeedb9f1f0e06a3bfc7e67de16717f36
thedocbwarren <jedi.master@augmentedjs.com>
2019-06-29 15:04:23 -0700

    adding controls and starting heatmap function

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const MAP_API_KEY = "do-not-use";
+export const MAP_API_KEY = "";
 export const GOOGLE_MAP_URL = "https://maps.googleapis.com/maps/api/geocode/json";
 export const APP_NAME = "appHazard";
 export const AUTHOR = "Karen Calpo &amp; Bob Warren";
