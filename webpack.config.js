c5450530f4cf80dc606497bd100262055237882b
thedocbwarren <jedi.master@augmentedjs.com>
2019-06-29 12:11:39 -0700

    fixes to base config

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,16 @@ const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+//const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: ['./src/index.js'],
   context: __dirname,
   target: "web",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    filename: "bundle.js",
+    chunkFilename: '[name].js'
   },
   module: {
     rules: [
@@ -24,30 +26,36 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
-          "file-loader"
+          { "loader": "file-loader",
+          options: {
+            name: '[name].[ext]',
+          }}
         ]
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2)$/,
         use: [
-          "file-loader"
+          { "loader": "file-loader",
+          options: {
+            name: '[name].[ext]',
+          }}
         ]
       },
       {
         test: /\.(css|scss)$/,
         use: [{
-            loader: "style-loader"
+          loader: "style-loader"
         },
         MiniCssExtractPlugin.loader,
         {
-            loader: "css-loader", options: {
-                sourceMap: true
-            }
+          loader: "css-loader", options: {
+            sourceMap: true
+          }
         },
         {
-            loader: "sass-loader", options: {
-                sourceMap: true
-            }
+          loader: "sass-loader", options: {
+            sourceMap: true
+          }
         }]
       }
     ]
@@ -62,10 +70,32 @@ module.exports = {
       favicon: "./src/images/favicon-32x32.png"
     }),
     new MiniCssExtractPlugin({
-        // Options similar to the same options in webpackOptions.output
-        // both options are optional
-        filename: "[name].css",
-        chunkFilename: "[id].css"
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
     })
-  ]
+    /*,
+    new BundleAnalyzerPlugin()*/
+  ],
+  // optimization
+  optimization: {
+    namedModules: true,
+    namedChunks: true,
+    splitChunks: {
+      cacheGroups: {
+        default: false,
+        vendors: false,
+
+        // vendor chunk
+        vendor: {
+          // sync + async chunks
+          chunks: 'all',
+
+          // import file path containing node_modules
+          test: /node_modules/
+        }
+      }
+    }
+  }
 };
