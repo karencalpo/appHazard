4d27df5512292e5bf57753c53b2f08e1db42a0a7
karencalpo <minnkc@gmail.com>
2019-06-29 16:10:25 -0700

    Code refectoring.

--- service/routes/property_locations.js
+++ service/routes/property_locations.js
@@ -1,5 +1,4 @@
-const asyncCall = require("../functions/asyncCall.js");
-const Risk = require("../functions/calcRisk.js");
+const calcData = require("../functions/calcData.js");
 
 module.exports = (app) => {
   app.get("/properties", (req, res) => {
@@ -36,100 +35,7 @@ module.exports = (app) => {
     const headers = [ATTOM_PROPERTIES_DATA, FEMA_DATA];
 
     if(req.query.address && req.query.city && req.query.state) {
-
-      const calculated_data = async () => {
-        try {
-          let allData = {}
-          let riskData = {};
-          let totalDisasters = {};
-          let locations = [];
-          let worst_disasters = [];
-          let total_disasters = [];
-          let propertyVals = [];
-          let propertyAvg = 0;
-          let greatest_disaster = 0;
-          const [attom, fema] = await Promise.all(headers.map(asyncCall));
-          if(attom["property"]) {
-            attom["property"].forEach((property) => {
-              propertyVals.push(property["assessment"]["assessed"]["assdttlvalue"]);
-            });
-
-            let propertyValsSorted = propertyVals.sort((a,b) => {
-              return a - b;
-            })
-
-            if((propertyValsSorted.length % 2) === 0) {
-              propertyAvg = (propertyValsSorted[(propertyValsSorted.length/2)-1] + propertyValsSorted[(propertyValsSorted.length/2)])/2;
-            } else {
-              propertyAvg = propertyValsSorted[Math.floor(propertyValsSorted.length/2)];
-            }
-          };
-
-          fema["DisasterDeclarationsSummaries"].forEach((disaster) => {
-
-              Object.keys(Risk.DISASTER_TYPE).forEach((type) => {
-                if(disaster["incidentType"].toLowerCase().includes(type.toLowerCase())) {
-                  if(totalDisasters[type]) {
-                    totalDisasters[type]++;
-                  } else {
-                    totalDisasters[type] = 1;
-                  }
-                }
-              });
-
-          });
-
-          if(Object.keys(totalDisasters).length !== 0) {
-            greatest_disaster = Object.keys(totalDisasters).reduce((a, b) => totalDisasters[a] > totalDisasters[b] ? a : b);
-
-
-            for(key in totalDisasters) {
-              if(totalDisasters[key] === totalDisasters[greatest_disaster]) {
-                worst_disasters.push(key);
-                total_disasters.push(key);
-              } else {
-                total_disasters.push(key);
-              }
-            }
-
-          } else {
-            greatest_disaster = 0;
-          }
-
-          if(attom["property"]) {
-            attom["property"].forEach((property) => {
-              let risk = Risk.calcRisk(property["assessment"]["assessed"]["assdttlvalue"], propertyAvg, fema["DisasterDeclarationsSummaries"].length, Risk.DISASTER_TYPE[greatest_disaster], Risk.RISK_DEMO["NORMAL"]);
-              locations.push({
-                lat: property["location"]["latitude"],
-                long: property["location"]["longitude"],
-                weight: risk
-              });
-            });
-          };
-
-          riskData = {
-            greatest_disasters: worst_disasters,
-            highest_value: totalDisasters[greatest_disaster],
-            types: total_disasters
-          };
-
-          allData = {
-            riskData: riskData,
-            locations: locations
-          }
-
-          res.send(allData);
-          return allData;
-          //res.send([attom, fema]);
-          //return [attom, fema];
-        } catch (e) {
-          console.error(e);
-          res.status(500).send(e);
-          return e;
-        }
-      };
-
-      calculated_data();
+      calcData(headers, req, res);
     } else if(req.query.city) {
       const results = [];
       for(let i = 0; i < data.length; i++) {
