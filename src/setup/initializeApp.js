4cd96a14c3065237b1fbd88ecaba2a73ed9827da
thedocbwarren <jedi.master@augmentedjs.com>
2019-06-29 13:02:21 -0700

    adding application class

--- src/setup/initializeApp.js
+++ src/setup/initializeApp.js
@@ -1,9 +1,16 @@
 import { AUTHOR, APP_NAME } from "../constants.js";
+import Application from "../application/application.js";
 
 const initializeApp = async () => {
   try {
     document.write(`<p>Hello from ${AUTHOR}!  This is ${APP_NAME} version ${VERSION}.</p>`);
     console.log("Hello!");
+
+    const p = await Application.start();
+    if (!p) {
+      throw new Error("Error starting application!");
+    }
+    
   } catch(e) {
     const err = `Error initializing Application - ${e}`;
     throw new Error(err);
