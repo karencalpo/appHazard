a87f3aff37378c2eb69cfd7c74a7487ffebd8a41
karencalpo <minnkc@gmail.com>
2019-06-29 18:24:38 -0700

    Added constants to property.js

--- service/routes/property.js
+++ service/routes/property.js
@@ -1,18 +1,15 @@
 // Helper Functions
 const https = require("https");
 const Logger = require("../logger.js");
+const CONSTANTS = require("../constants.js");
 
 module.exports = (app) => {
-  app.post("/single_property", (app_req, app_res) => {
-    const single_property_url = encodeURI(`/propertyapi/v1.0.0/attomavm/detail?address1=${app_req.body.address}&address2=${app_req.body.city}`);
-    const ATTOM_SINGLE_PROPERTY = {
-      hostname: `api.gateway.attomdata.com`,
-      port: 443,
-      path: single_property_url,
-      headers: { accept: `application/json`, apikey: `ef4e2dafba28149f3f3c987e3a1ea57c`}
-    };
-    console.log(app_req.body);
-    if(app_req.body.address && app_req.body.city) {
+  app.post("/property", (req, res) => {
+    const property_url = encodeURI(`/propertyapi/v1.0.0/attomavm/detail?address1=${req.body.address}&address2=${req.body.city}`);
+    const ATTOM_SINGLE_PROPERTY = CONSTANTS.ATTOM_PROPERTIES_DATA;
+    ATTOM_SINGLE_PROPERTY["path"] = property_url;
+
+    if(req.body.address && req.body.city) {
       https.get(ATTOM_SINGLE_PROPERTY, (response) => {
         let stream = "";
 
@@ -23,20 +20,20 @@ module.exports = (app) => {
         response.on("end", () => {
           try {
             const data = JSON.parse(stream);
-            app_res.send(data.property[0]);
+            res.send(data.property[0]);
           } catch(e) {
             Logger.error(e);
-            app_res.status(500).send(e.message);
+            res.status(500).send(e.message);
           }
 
         }).on(`error`, (e) => {
             Logger.error(e);
-            app_res.status(500).send(e.message);
+            res.status(500).send(e.message);
         });
 
       });
     } else {
-      app_res.status(400).send("Requires address and city.");
+      res.status(400).send("Requires address and city.");
     }
   });
 
